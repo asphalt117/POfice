069a0ac0291d8961ce6564bc10d866bb2c5e517d
DiMoisha <165@list.ru>
2020-02-21 16:02:31 +0300

    Много разного

---
+++ WebUI/Helper/ComponentsHelpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Web.Mvc.Html;
+using WebUI.Models;
+
+namespace WebUI.Helpers
+{
+    public static class ComponentsHelpers
+    {
+        public static MvcHtmlString ButtonAdd(this HtmlHelper helper, string title, string action, string controller, string prms)
+        {
+            StringBuilder viewString = new StringBuilder();          // HTML - строка
+
+            viewString.Append("<p><a class=\"btn btn-light\" role=\"button\" title=\"" + title);
+            viewString.Append("\" href=\"@Url.Action(\"" + action);
+            viewString.Append("\", \"" + controller);
+            viewString.Append("\", " + prms + ")>");
+            viewString.Append("<i class=\"fa fa-plus-square\"></i>&nbsp;&nbsp;Создать</a></p>");
+
+            return MvcHtmlString.Create(viewString.ToString());
+        }
+
+        public static MvcHtmlString ButtonReturn(this HtmlHelper helper, string title, string action, string controller, string prms, string caption)
+        {
+            StringBuilder viewString = new StringBuilder();          // HTML - строка
+
+            viewString.Append("<p><a class=\"btn btn-light\" role=\"button\" title=\"" + title);
+            viewString.Append("\" href=\"@Url.Action(\"" + action);
+            viewString.Append("\", \"" + controller);
+            viewString.Append((prms != null ? "\", " + prms : "") + ")>");
+            viewString.Append("<i class=\"fa fa-angle-double-left\"></i>&nbsp;&nbsp;" + caption + "</a></p>");
+
+            return MvcHtmlString.Create(viewString.ToString());
+        }
+    }
+}
---
+++ WebUI/Helper/TabHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Web.Mvc.Html;
+using WebUI.Models;
+
+namespace WebUI.Helpers
+{
+    public static class TabHelpers
+    {
+        public static MvcHtmlString TableHeader(this HtmlHelper helper,  List<string> headers)
+        {
+            StringBuilder viewString = new StringBuilder();          // HTML - строка
+            if (headers.Count > 0) 
+            {
+                viewString.Append("<table class=\"table table-bordered table-hover mt-3\"><thead><tr class=\"table-active\">");
+
+                foreach (string header in headers)
+                {
+                    viewString.Append("<th scope=\"col\">" + header + "</th>");
+                }
+      
+                viewString.Append("</tr></thead><tbody>");
+            }
+             
+            return MvcHtmlString.Create(viewString.ToString());
+        }
+
+        public static MvcHtmlString TableEnd(this HtmlHelper helper) 
+        {
+            return MvcHtmlString.Create("</tbody></table>");
+        }
+    }
+}
