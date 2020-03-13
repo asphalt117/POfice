d366166e03413115ef8c79f82faef42c9b413b5b
DiMoisha <165@list.ru>
2020-03-13 13:22:48 +0300

    ыаппвап

---
+++ WebUI/Helper/MenuHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Web.Mvc.Html;
+using WebUI.Models;
+
+namespace WebUI.Helpers
+{
+    public static class MenuHelpers
+    {
+        public static MvcHtmlString MenuItemLink(this HtmlHelper helper, string caption, string url)
+        {
+            StringBuilder viewString = new StringBuilder();
+            viewString.Append("<a class=\"nav-link\" role=\"button\" href=\"" + url + "\">");
+            viewString.Append(caption);
+            viewString.Append("<span class=\"chevron-item-wrapper\"><i class=\"chevron-item\"></i></span></a>");                                       
+            return MvcHtmlString.Create(viewString.ToString());
+        }
+    }
+}
